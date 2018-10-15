0fa712d566b9f499aa849559f61b209c879775b1
Igor D <igorderik@yahoo.com>
2018-10-15 23:04:57 +0300

    sparksession added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
@@ -6,6 +13,7 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.spark.sql.SparkSession;
 
 public class Main {
 
@@ -20,9 +28,12 @@ public class Main {
         //matchRDD.collect().forEach( match -> System.out.println(match.getCity()) );
         //JavaRDD<Match> fiftiesRDD = matchRDD.filter( match -> match.getYear()>1949 && match.getYear()<1960 );
         //System.out.println(fiftiesRDD.count());
+        //JavaRDD<Match> yokohamaRDD = matchRDD.filter( m -> m.getCity().equals("Yokohama ") );
+        //System.out.println(yokohamaRDD.count());
 
-        JavaRDD<Match> yokohamaRDD = matchRDD.filter( m -> m.getCity().equals("Yokohama ") );
-        System.out.println(yokohamaRDD.count());
+        SparkSession spark = SparkSession.builder().getOrCreate();
+        Dataset<Row> matchDF = spark.createDataFrame(matchRDD,Match.class);
+        matchDF.show(1000);
 
     }
 }
