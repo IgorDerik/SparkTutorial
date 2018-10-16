b4fa31670c136d949d968f19d953f74c96b6d6f9
Igor D <igorderik@yahoo.com>
2018-10-16 11:04:47 +0300

    Maked changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,16 +24,25 @@ public class Main {
 
         JavaRDD<String> stringRDD = context.textFile("src/main/resources/WorldCupMatches.csv");
         JavaRDD<Match> matchRDD = stringRDD.map(Match::createMatch);
-
+        long allMatchesCount = matchRDD.count();
         //matchRDD.collect().forEach( match -> System.out.println(match.getCity()) );
         //JavaRDD<Match> fiftiesRDD = matchRDD.filter( match -> match.getYear()>1949 && match.getYear()<1960 );
         //System.out.println(fiftiesRDD.count());
-        //JavaRDD<Match> yokohamaRDD = matchRDD.filter( m -> m.getCity().equals("Yokohama ") );
-        //System.out.println(yokohamaRDD.count());
+        JavaRDD<Match> moreThan4goalsMatchesRDD = matchRDD.filter( match -> match.getHomeTeamGoals()>4 );
+        long moreThan4goalsMatchesCount = moreThan4goalsMatchesRDD.count();
+        String message = "Percentage of matches with more than 4 goals: ";
+        System.out.println( message + ((double) moreThan4goalsMatchesCount/allMatchesCount)*100 + "%");
+        //System.out.println(moreThan4goalsMatchesCount);
+        //System.out.println(allMatchesCount);
+        //moreThan4goalsMatches.collect().forEach(System.out::println);
 
-        SparkSession spark = SparkSession.builder().getOrCreate();
-        Dataset<Row> matchDF = spark.createDataFrame(matchRDD,Match.class);
-        matchDF.show(1000);
+        //SparkSession spark = SparkSession.builder().getOrCreate();
+        //Dataset<Row> matchDF = spark.createDataFrame(matchRDD,Match.class);
+        //matchDF.show(1000);
+        //matchDF.createOrReplaceTempView("matches");
+        //Dataset<Row> sqlDF = spark.sql("SELECT homeTeamGoals FROM matches where homeTeamGoals>5");
+        //Dataset<Row> sqlDF = spark.sql("");
+        //sqlDF.show(100);
 
     }
 }
